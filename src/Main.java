60e9b3b5ce9715f4ccdfbbcf31143a772a5372e1
antonelis <anton.elis@hotmail.com>
2021-09-30 18:12:00 +0200

    Check for nullPointer if movie does not exist + cleaning in Main method

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,28 @@ import java.util.Arrays;
 
 public class Main {
 
-  public static void main(String[] args) {
-    String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+    public static void main(String[] args) {
+      String expected = expectedString();
 
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+      String result = resultString();
 
-    if (!result.equals(expected)) {
-      throw new AssertionErrorException(expected, result);
+      if (!result.equals(expected)) {
+            throw new AssertionErrorException(expected, result);
+        }
+
+        System.out.println("Success");
     }
 
-    System.out.println("Success");
+  private static String resultString() {
+    return new RentalInfo().statement(
+                new Customer("C. U. Stomer", Arrays.asList(
+                        new MovieRental("F001", 3),
+                        new MovieRental("F002", 1))));
+  }
+
+  private static String expectedString() {
+    return "Rental Record for C. U. Stomer\n\t"
+                + "You've Got Mail\t3.5\n\tMatrix\t2.0\n"
+                + "Amount owed is 5.5\nYou earned 2 frequent points\n";
   }
 }
--- src/RentalInfo.java
+++ src/RentalInfo.java
@@ -2,56 +2,60 @@ import java.util.HashMap;
 
 public class RentalInfo {
 
-  public static HashMap<String, Movie> generateMovies() {
+    public static HashMap<String, Movie> generateMovies() {
 
-    HashMap<String, Movie> movies = new HashMap<>();
-    movies.put("F001", new Movie("You've Got Mail", MovieCategory.REGULAR));
-    movies.put("F002", new Movie("Matrix", MovieCategory.REGULAR));
-    movies.put("F003", new Movie("Cars", MovieCategory.CHILDRENS));
-    movies.put("F004", new Movie("Fast & Furious X", MovieCategory.NEW));
+        HashMap<String, Movie> movies = new HashMap<>();
+        movies.put("F001", new Movie("You've Got Mail", MovieCategory.REGULAR));
+        movies.put("F002", new Movie("Matrix", MovieCategory.REGULAR));
+        movies.put("F003", new Movie("Cars", MovieCategory.CHILDRENS));
+        movies.put("F004", new Movie("Fast & Furious X", MovieCategory.NEW));
 
-    return movies;
-  }
-
-  public static HashMap<MovieCategory, IAmount> movieCategoryAmount() {
-    HashMap<MovieCategory, IAmount> amountHashMap = new HashMap<>();
-    amountHashMap.put(MovieCategory.REGULAR, new RegularMovieAmount());
-    amountHashMap.put(MovieCategory.CHILDRENS, new ChildrenMovieAmount());
-    amountHashMap.put(MovieCategory.NEW, new NewMovieAmount());
-
-    return amountHashMap;
-  }
-
-  public static HashMap<MovieCategory, IBonus> movieCategoryBonus() {
-    HashMap<MovieCategory, IBonus> bonusesHashMap = new HashMap<>();
-    bonusesHashMap.put(MovieCategory.REGULAR, new RegularMovieAmount());
-    bonusesHashMap.put(MovieCategory.CHILDRENS, new ChildrenMovieAmount());
-    bonusesHashMap.put(MovieCategory.NEW, new NewMovieAmount());
-
-    return bonusesHashMap;
-  }
-
-  public String statement(Customer customer) {
-
-    double totalAmount = 0;
-    int frequentEnterPoints = 0;
-    String result = "Rental Record for " + customer.getName() + "\n";
-
-    for (MovieRental rental : customer.getRentals()) {
-
-      double thisAmount = movieCategoryAmount().get(generateMovies().get(rental.getMovieId()).getCategory()).getAmount(rental.getDays());
-
-      //add frequent bonus points
-      frequentEnterPoints += movieCategoryBonus().get(generateMovies().get(rental.getMovieId()).getCategory()).bonus(rental.getDays());
-
-      //print figures for this rental
-      result += "\t" + generateMovies().get(rental.getMovieId()).getTitle() + "\t" + thisAmount + "\n";
-      totalAmount = totalAmount + thisAmount;
+        return movies;
     }
-    // add footer lines
-    result += "Amount owed is " + totalAmount + "\n";
-    result += "You earned " + frequentEnterPoints + " frequent points\n";
 
-    return result;
-  }
+    public static HashMap<MovieCategory, IAmount> movieCategoryAmount() {
+        HashMap<MovieCategory, IAmount> amountHashMap = new HashMap<>();
+        amountHashMap.put(MovieCategory.REGULAR, new RegularMovieAmount());
+        amountHashMap.put(MovieCategory.CHILDRENS, new ChildrenMovieAmount());
+        amountHashMap.put(MovieCategory.NEW, new NewMovieAmount());
+
+        return amountHashMap;
+    }
+
+    public static HashMap<MovieCategory, IBonus> movieCategoryBonus() {
+        HashMap<MovieCategory, IBonus> bonusesHashMap = new HashMap<>();
+        bonusesHashMap.put(MovieCategory.REGULAR, new RegularMovieAmount());
+        bonusesHashMap.put(MovieCategory.CHILDRENS, new ChildrenMovieAmount());
+        bonusesHashMap.put(MovieCategory.NEW, new NewMovieAmount());
+
+        return bonusesHashMap;
+    }
+
+    public String statement(Customer customer) {
+
+        double totalAmount = 0;
+        int frequentEnterPoints = 0;
+        StringBuilder result = new StringBuilder("Rental Record for " + customer.getName() + "\n");
+
+        for (MovieRental rental : customer.getRentals()) {
+            try {
+                double thisAmount = movieCategoryAmount().get(generateMovies().get(rental.getMovieId()).getCategory()).getAmount(rental.getDays());
+
+                //add frequent bonus points
+                frequentEnterPoints += movieCategoryBonus().get(generateMovies().get(rental.getMovieId()).getCategory()).bonus(rental.getDays());
+
+                //print figures for this rental
+                result.append("\t").append(generateMovies().get(rental.getMovieId()).getTitle()).append("\t").append(thisAmount).append("\n");
+                totalAmount = totalAmount + thisAmount;
+            } catch (NullPointerException ex) {
+                System.out.println("This movie does not exist");
+                ex.printStackTrace();
+            }
+        }
+        // add footer lines
+        result.append("Amount owed is ").append(totalAmount).append("\n");
+        result.append("You earned ").append(frequentEnterPoints).append(" frequent points\n");
+
+        return result.toString();
+    }
 }
