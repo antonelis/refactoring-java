030f3a4d754ccd5676cf0d4a88aeefbea19c734c
antonelis <anton.elis@hotmail.com>
2021-09-30 17:52:00 +0200

    Refactoring statement method. Moving out logic in own classes and implementing interfaceAmount.

--- src/RentalInfo.java
+++ src/RentalInfo.java
@@ -13,40 +13,33 @@ public class RentalInfo {
     return movies;
   }
 
+  public static HashMap<MovieCategory, IAmount> movieCategoryAmount() {
+    HashMap<MovieCategory, IAmount> amountHashMap = new HashMap<>();
+    amountHashMap.put(MovieCategory.REGULAR, new RegularMovieAmount());
+    amountHashMap.put(MovieCategory.CHILDRENS, new ChildrenMovieAmount());
+    amountHashMap.put(MovieCategory.NEW, new NewMovieAmount());
+
+    return amountHashMap;
+  }
+
 
   public String statement(Customer customer) {
-
 
     double totalAmount = 0;
     int frequentEnterPoints = 0;
     String result = "Rental Record for " + customer.getName() + "\n";
-    for (MovieRental r : customer.getRentals()) {
-      double thisAmount = 0;
 
-      // determine amount for each movie
-      if (generateMovies().get(r.getMovieId()).getCategory().equals(MovieCategory.REGULAR)) {
-        thisAmount = 2;
-        if (r.getDays() > 2) {
-          thisAmount = ((r.getDays() - 2) * 1.5) + thisAmount;
-        }
-      }
-      if (generateMovies().get(r.getMovieId()).getCategory().equals(MovieCategory.NEW)) {
-        thisAmount = r.getDays() * 3;
-      }
-      if (generateMovies().get(r.getMovieId()).getCategory().equals(MovieCategory.CHILDRENS)) {
-        thisAmount = 1.5;
-        if (r.getDays() > 3) {
-          thisAmount = ((r.getDays() - 3) * 1.5) + thisAmount;
-        }
-      }
+    for (MovieRental rental : customer.getRentals()) {
+
+      double thisAmount = movieCategoryAmount().get(generateMovies().get(rental.getMovieId()).getCategory()).getAmount(rental.getDays());
 
       //add frequent bonus points
       frequentEnterPoints++;
       // add bonus for a two day new release rental
-      if (generateMovies().get(r.getMovieId()).getCategory() == MovieCategory.NEW && r.getDays() > 2) frequentEnterPoints++;
+      if (generateMovies().get(rental.getMovieId()).getCategory() == MovieCategory.NEW && rental.getDays() > 2) frequentEnterPoints++;
 
       //print figures for this rental
-      result += "\t" + generateMovies().get(r.getMovieId()).getTitle() + "\t" + thisAmount + "\n";
+      result += "\t" + generateMovies().get(rental.getMovieId()).getTitle() + "\t" + thisAmount + "\n";
       totalAmount = totalAmount + thisAmount;
     }
     // add footer lines
